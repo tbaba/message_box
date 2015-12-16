e7500e81a1dc1fe6b366676e4b6f88e004153b4a
Tatsuro Baba <harakirisoul@gmail.com>
2015-12-16 21:18:59 +0900

    Set top#index as root uri

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'top/index'
+  root 'top#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
